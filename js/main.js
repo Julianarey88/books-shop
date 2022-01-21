4e00310f29287023a687eb1234b5bba0afd53659
alienat3d <psy.alienated@gmail.com>
2022-01-21 05:55:19 +0300

    finished promoi-slider

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+  $('.promo-slider__wrapper').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay: false,
+    speed: 700,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="img/icons/anlge-left.svg" alt="стрелка влево"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="img/icons/anlge-right.svg" alt="стрелка вправо"></button>',
+  });
+});
